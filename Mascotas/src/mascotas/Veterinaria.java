e392ef5f39995ff6b6ee81355decb47c062c3cff
btitán <bryanmat65@gmail.com>
2025-07-15 22:43:31 -0600

    actualizacion

--- Mascotas/src/mascotas/Veterinaria.java
+++ Mascotas/src/mascotas/Veterinaria.java
@@ -1,8 +1,8 @@
 public class Veterinaria {
     public static void main(String[] args) {
         // Crear objetos de tipo Mascota
-        Mascotas mascota1 = new Mascotas("KIRA", "Perro", 5, true);
-        Mascotas mascota2 = new Mascotas("pelusa", "Gato", 3, false);
+        Mascotas mascota1 = new Mascotas("toby", "Perro", 5, true);
+        Mascotas mascota2 = new Mascotas("lolis", "Gato", 3, false);
         Mascotas mascota3 = new Mascotas("Tai lung", "Gato", 2, true);
 
         // Mostrar información de cada mascota
